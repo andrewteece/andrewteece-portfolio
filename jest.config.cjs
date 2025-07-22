9e3e23f0fb479a7fd9e2506fcc360e7979aae1fb
andrewteece <andrewteece@gmail.com>
2025-07-22 09:07:28 -0500

    jest-jsdom install in jest.config updated

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
