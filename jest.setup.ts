29f3b909898971454998ff43e4d9d26d74d6809a
andrewteece <andrewteece@gmail.com>
2025-07-22 09:44:08 -0500

    install node types update jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+// Workaround for ESM module environments
+(async () => {
+  const util = await import('node:util');
+
+  // @ts-expect-error TS doesn't know this is safe in Node + Jest
+  globalThis.TextEncoder = util.TextEncoder;
+
+  // @ts-expect-error
+  globalThis.TextDecoder = util.TextDecoder;
+})();
