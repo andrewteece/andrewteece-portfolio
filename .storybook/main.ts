83b307178249b24a8973d0c916fe4e45cdf95577
andrewteece <andrewteece@gmail.com>
2025-08-09 07:59:41 -0500

    refactor to fix mdx error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    // Only load Storybook stories (CSF + MDX stories)
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/**/*.stories.mdx',
+    // Explicitly exclude site/blog MDX so SB doesn't try to parse it
+    '!../src/content/**/*.mdx',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
 };
+
 export default config;
