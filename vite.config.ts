42d9b1695f7831c5e3b78586b82d798c1cf9e3a4
andrewteece <andrewteece@gmail.com>
2025-08-09 07:44:41 -0500

    install storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,20 +7,19 @@ import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import { configDefaults } from 'vitest/config';
 import remarkGfm from 'remark-gfm';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    mdx({
-      providerImportSource: '@mdx-js/react',
-      remarkPlugins: [
-        remarkGfm,
-        remarkFrontmatter,
-        [remarkMdxFrontmatter, { name: 'frontmatter' }],
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss(), mdx({
+    providerImportSource: '@mdx-js/react',
+    remarkPlugins: [remarkGfm, remarkFrontmatter, [remarkMdxFrontmatter, {
+      name: 'frontmatter'
+    }]]
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -27,7 +27,28 @@ export default defineConfig({
     exclude: [...configDefaults.exclude, 'dist', 'coverage'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html', 'clover'],
+      reporter: ['text', 'json', 'html', 'clover']
     },
-  },
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
